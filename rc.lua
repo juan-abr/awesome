e186d3bcd2a1c3e6a035665b351c0741a0a7e211
Juan R <juanabarro@protonmail.com>
2025-05-30 12:06:13 -0400

    mapped Pixelorama to tag

--- rc.lua
+++ rc.lua
@@ -534,8 +534,8 @@ awful.rules.rules = {
     -- Set Firefox to always map on the tag named "2" on screen 1.
     { rule = { class = "Alacritty" }, properties = { screen = 1, tag = "" } },
     { rule = { class = "Xfce4-terminal" }, properties = { screen = 1, tag = "" } },
-    { rule = { class = "Firefox" }, properties = { screen = 1, tag = "" } },
-    { rule = { class = "Chromium" }, properties = { screen = 1, tag = "" } },
+    { rule = { class = "Firefox" }, properties = { screen = 1, tag = "" } },
+    { rule = { class = "Pixelorama" }, properties = { screen = 1, tag = "" } },
 }
 -- }}}
 
@@ -606,36 +606,3 @@ client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_n
 -- }}}
 
 awful.spawn.with_shell("~/.config/awesome/autorun.sh")
-
--- battery warning
--- created by bpdp
--- https://bpdp.blogspot.com/2013/06/battery-warning-notification-for.html
-
-local function trim(s)
-    return s:find'^%s*$' and '' or s:match'^%s*(.*%S)'
-end
-
-local function bat_notification()
-
-    local f_capacity = assert(io.open("/sys/class/power_supply/BAT0/capacity", "r"))
-    local f_status = assert(io.open("/sys/class/power_supply/BAT0/status", "r"))
-
-    local bat_capacity = tonumber(f_capacity:read("*all"))
-    local bat_status = trim(f_status:read("*all"))
-
-    if (bat_capacity <= 15 and bat_status == "Discharging") then
-        naughty.notify({ title      = "Battery Warning"
-            , text       = "Battery low! " .. bat_capacity .."%" .. " left!"
-            , fg="#ff0000"
-            , bg="#deb887"
-            , timeout    = 15
-            , position   = "bottom_left"
-        })
-    end
-end
-
-battimer = timer({timeout = 120})
-battimer:connect_signal("timeout", bat_notification)
-battimer:start()
-
--- end here for battery warning
